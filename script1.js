0af9b2a471b91784d2f8e06769b66548cd1329ec
Sephiroth <78663409+sephirothcoding@users.noreply.github.com>
2022-12-04 12:20:36 +0200

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,5 +1,9 @@
 
-
+if ( navigator.platform.indexOf('Win') != -1 ) {
+  window.document.getElementById("wrapper").setAttribute("class", "windows");
+} else if ( navigator.platform.indexOf('Mac') != -1 ) {
+  window.document.getElementById("wrapper").setAttribute("class", "mac");
+}
 
 
 ///////////
